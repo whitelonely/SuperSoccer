e4b264e5c8dadc8ca5731f35bd2f8321054e3fbc
whitelonely <JNwhitelonely@gmail.com>
2025-06-14 19:57:40 +0800

    实现运球状态更真实化效果

--- scripts/ball_states/ball_state.gd
+++ scripts/ball_states/ball_state.gd
@@ -1,13 +1,18 @@
 class_name BallState
 extends Node
 
-signal state_transition_requested(new_state: BallState)
+signal state_transition_requested(new_state: Ball.State)
 
+var animation_player : AnimationPlayer = null
 var ball : Ball = null
 var carrier : Player = null
 var player_detection_area : Area2D = null
 
-func setup(context_ball: Ball, context_player_detection_area: Area2D, context_carrier: Player) -> void:
+func setup(
+	context_ball: Ball, context_player_detection_area: Area2D, 
+	context_carrier: Player, context_animation_player: AnimationPlayer
+	) -> void:
 	ball = context_ball
 	player_detection_area = context_player_detection_area
 	carrier = context_carrier
+	animation_player = context_animation_player
--- scripts/ball_states/ball_state_carried.gd
+++ scripts/ball_states/ball_state_carried.gd
@@ -1,8 +1,30 @@
 class_name BallStateCarried
 extends BallState
 
+const OFFSET_FROM_PLAYER := Vector2(10, 4)
+# 运球更真实优化
+const DRIBBLE_FREQUENCY := 10.0	#运球频率
+const DRIBBLE_INTENSITY := 3.0	#运球强度
+
+var dribble_time := 0.0
+
 func _enter_tree() -> void:
 	assert(carrier != null)
 
-func _process(_delta: float) -> void:
-	ball.position = carrier.position
+func _process(delta: float) -> void:
+	var vx = 0.0
+	dribble_time += delta
+	
+	if carrier.velocity != Vector2.ZERO:
+		if carrier.velocity.x != 0:
+			vx = cos(dribble_time * DRIBBLE_FREQUENCY) * DRIBBLE_INTENSITY
+		if carrier.heading.x >= 0:
+			animation_player.play("roll")
+			animation_player.advance(0)
+		else:
+			animation_player.play_backwards("roll")
+			animation_player.advance(0)
+	else:
+		animation_player.play("idle")
+	ball.position = carrier.position + Vector2(vx + carrier.heading.x * OFFSET_FROM_PLAYER.x, OFFSET_FROM_PLAYER.y)
+	
